0245b70f18f95ac90153dc9eedfaddf91b69995f
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-01-09 00:46:04 +0600

    until blue commit

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { blueApi, testGetApi } from "../controllers/api.controller.js";
+import { blueApi, testGetApi, untilApi } from "../controllers/api.controller.js";
 const router = express.Router();
 
 router.get("/test", testGetApi);
 router.get("/blue", blueApi);
+router.get("/until", untilApi);
 
 export default router;
